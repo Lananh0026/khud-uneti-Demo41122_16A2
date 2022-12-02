fd7a520ef13731e40013de8b988580c801bf1d56
Nguyễn Thị Lan Anh <117256673+Lananh0026@users.noreply.github.com>
2022-12-02 08:57:58 +0700

    Add files via upload

---
+++ Bai9.6.py
@@ -0,0 +1,10 @@
+n=int(input('Nhập vào số nguyên dương:'))
+dem=0
+for i in range(1,n+1):
+    if n % i ==0:
+        dem+=1
+if dem==2:
+        print(n,'Là số nguyên tố')        
+else:
+        print('Không là số nguyên tố',n)
+   
