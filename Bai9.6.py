a92089fa192c4567fc0917133561ef2669dc81e4
Nguyễn Thị Lan Anh <117256673+Lananh0026@users.noreply.github.com>
2022-12-02 09:39:40 +0700

    Update Bai9.6.py

--- Bai9.6.py
+++ Bai9.6.py
@@ -4,7 +4,9 @@ for i in range(1,n+1):
     if n % i ==0:
         dem+=1
 if dem==2:
-        print(n,'Là số nguyên tố')        
+        print(n,'Là số nguyên tố')     
+        print(True)
 else:
         print('Không là số nguyên tố',n)
+        print(False)
    
