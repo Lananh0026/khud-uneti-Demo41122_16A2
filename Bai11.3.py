0fecabd5f6489c8f79d78457a0fb1775e48d87a8
Nguyễn Thị Lan Anh <117256673+Lananh0026@users.noreply.github.com>
2022-12-14 10:48:07 +0700

    Add files via upload

---
+++ Bai11.3.py
@@ -0,0 +1,6 @@
+list_animals=['ant','bear','cat','dog','elphant','fish','goat','hippo']
+print(list_animals)
+print('number of animals',len(list_animals))
+x=str(input('I want to find:'))
+find= x in list_animals
+print('There is',find,'in list animals') 
