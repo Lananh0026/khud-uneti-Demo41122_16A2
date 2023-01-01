bed0493484e316d50443d18e5113d2fc7d2b45b2
Nguyễn Thị Lan Anh <117256673+Lananh0026@users.noreply.github.com>
2023-01-01 18:10:31 +0700

    Update Bai11.6.py

--- Bai11.6.py
+++ Bai11.6.py
@@ -4,3 +4,18 @@ def chuyen_doi(x):
     return cd
 list_cd=list(map(chuyen_doi, list1))
 print('List sau khi đổi từ inch sang mét:',list_cd)
+#in ra 3 chiều cao đầu tiên và 3 chiều cao cuối cùng
+print('3 chiều cao đầu tiên là:',list_cd[0:3])
+print('3 chiều cao cuối cùng là:',list_cd[6:9])
+#Chiều cao trung bình, chiều cao lớn nhất, chiều cao nhỏ nhất
+tb=sum(list_cd)/len(list_cd)
+print('Chiều cao trung bình là:',tb)
+print('Chiều cao lớn nhất là:',max(list_cd))
+print('Chiều cao nhỏ nhất là:',min(list_cd))
+#Sắp xếp tăng dần
+list_cd.sort()
+print('Chiều cao tăng dần là:',list_cd)
+#Sắp xếp giảm dần
+list_cd.sort()
+list.reverse()
+print('Chiều cao sắp xếp giảm dần là:',list_cd)
